f8d521d7f4503ffd3dd6cff46010295c258069ad
lawcia <lawrencianjume@gmail.com>
2021-11-13 11:57:24 +0000

    first unit test\

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Cell:
+    def __init__(self, is_living):
+        self.is_living = is_living
+
+    def update_state(self, neighbours):
+        self.is_living = False
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ test_main.py
@@ -0,0 +1,16 @@
+import unittest
+from main import Cell
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_if_cell_has_0_neighbours_should_die(self):
+        cell = Cell(is_living=True)
+        cell.update_state(neighbours=0)
+        self.assertFalse(cell.is_living)
+
+
+if __name__ == '__main__':
+    unittest.main()
